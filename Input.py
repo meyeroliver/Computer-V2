25212cf4a37d6f50ed906a6cc0829785fd2ed884
omeyer <meyeroliver9401@gmail.com>
2018-10-16 10:20:00 +0200

    Creating user input

---
+++ Input.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    userInput = raw_input("> ")
+    while userInput != "exit":
+        print(userInput)
+        userInput = raw_input("> ")
+    print("Thank you. Come again.")
